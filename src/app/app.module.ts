d48b9f48d7e1028aed375e7390a1d393cdf5b4ca
aristotelesjunior <Tenchu88*>
2023-09-30 00:35:57 -0300

    Criados componentes que serão utilizados e finalizado o componente menu-title

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuBarComponent } from './components/menu-bar/menu-bar.component';
+import { MenuTitleComponent } from './components/menu-title/menu-title.component';
+import { SearchBarComponent } from './components/search-bar/search-bar.component';
+import { SubtitleComponent } from './components/subtitle/subtitle.component';
+import { BigCardComponent } from './components/big-card/big-card.component';
+import { SmallCardComponent } from './components/small-card/small-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuBarComponent,
+    MenuTitleComponent,
+    SearchBarComponent,
+    SubtitleComponent,
+    BigCardComponent,
+    SmallCardComponent
   ],
   imports: [
     BrowserModule,
